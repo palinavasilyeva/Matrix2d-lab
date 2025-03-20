bf9fcf2b303d32f8b40a75532907e5d0491e7503
palina vasilyeva <palina.vasilyeva@microsoft.wsei.edu.pl>
2025-03-20 13:15:06 +0100

    Program

--- Mtrix2dConsoleApp/Program.cs
+++ Mtrix2dConsoleApp/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Matrix2dLib;
+
+var m = new Matrix2d(1, 2, 3, 4);
+Console.WriteLine(m);
+
+int[,] a = { { 1, 2 }, { 3, 4 } };
+int[,] b = { { 5, 6 }, { 7, 8 } };
+
+Matrix2d m1 = a + (Matrix2d)b;
