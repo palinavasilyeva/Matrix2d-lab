e8c47f829158f9945077d90831f5743a1332b29d
palina vasilyeva <palina.vasilyeva@microsoft.wsei.edu.pl>
2025-03-20 12:44:32 +0100

    Testy

--- Matrix2dTest/Test1.cs
+++ Matrix2dTest/Test1.cs
@@ -1,11 +1,24 @@
-﻿namespace Matrix2dTest
+﻿using Matrix2dLib;
+
+namespace Matrix2dTest
 {
     [TestClass]
     public sealed class Test1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Konstructor_Wieloargumentowy_Poprawnie_OK()
         {
+            //Arrange
+            var m = new Matrix2d(1, 2, 3, 4);
+
+            //Act
+
+
+            //Assert
+            Assert.AreEqual(1, m.A);
+            Assert.AreEqual(2, m.B);
+            Assert.AreEqual(3, m.C);
+            Assert.AreEqual(4, m.D);
         }
     }
 }
