4d40c23657464946e8f5b796977e85fb0dc4ad55
palinavasilyeva <palina.vasilyeva@microsoft.wsei.edu.pl>
2025-03-21 15:42:04 +0100

    Det

--- Matrix2dTest/Test1.cs
+++ Matrix2dTest/Test1.cs
@@ -210,9 +210,10 @@ namespace Matrix2dTest
         }
 
         [TestMethod]
-        public void Test_Static_Det()
+        public void Test_Det()
         {
-            
+            var m = new Matrix2d(1, 2, 3, 4);
+            Assert.AreEqual(-2, m.Det());
         }
     }
 }
