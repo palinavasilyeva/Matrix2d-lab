74a67a456c9e088f68f7e101237caec3fcd640eb
palinavasilyeva <palina.vasilyeva@microsoft.wsei.edu.pl>
2025-03-21 14:53:47 +0100

    teste

--- Matrix2dTest/Test1.cs
+++ Matrix2dTest/Test1.cs
@@ -90,7 +90,7 @@ namespace Matrix2dTest
         public void Test_NotEquals_Operator()
         {
             var m1 = new Matrix2d(1, 2, 3, 4);
-            var m2 = new Matrix2d(1, 2, 3, 4);
+            var m2 = new Matrix2d(1, 2, 3, 5);
             Assert.IsTrue(m1 != m2);
         }
 
@@ -122,11 +122,61 @@ namespace Matrix2dTest
             Assert.AreEqual(0, result.A);
             Assert.AreEqual(0, result.B);
             Assert.AreEqual(0, result.C);
-            Assert.AreEqual(1, result.D);
+            Assert.AreEqual(-1, result.D);
             Assert.AreEqual(m1.A - m2.A, result.A);  
             Assert.AreEqual(m1.B - m2.B, result.B); 
             Assert.AreEqual(m1.C - m2.C, result.C); 
             Assert.AreEqual(m1.D - m2.D, result.D); 
+        }
+
+        [TestMethod]
+        public void Test_Multiply_Operator()
+        {
+            var m1 = new Matrix2d(1, 2, 3, 4);
+            var m2 = new Matrix2d(1, 2, 3, 5);
+            
+            var result = m1 * m2;
+            
+            Assert.AreEqual(7, result.A);
+            Assert.AreEqual(12, result.B);
+            Assert.AreEqual(15, result.C);
+            Assert.AreEqual(26, result.D);
+            Assert.AreEqual(m1.A * m2.A + m1.B * m2.C, result.A);
+            Assert.AreEqual(m1.A * m2.B + m1.B * m2.D, result.B);
+            Assert.AreEqual(m1.C * m2.A + m1.D * m2.C, result.C);
+            Assert.AreEqual(m1.C * m2.B + m1.D * m2.D, result.D);
+        }
+
+        [TestMethod]
+        public void Test_Multiply_Number_Left_Operator()
+        {
+            var m1 = new Matrix2d(1, 2, 3, 4);
+            var x = 2;
+            var m3 = m1 * x;
+            Assert.AreEqual(2, m3.A);
+            Assert.AreEqual(4, m3.B);
+            Assert.AreEqual(6, m3.C);
+            Assert.AreEqual(8, m3.D);
+            Assert.AreEqual(m1.A * x, m3.A);
+            Assert.AreEqual(m1.B * x, m3.B);
+            Assert.AreEqual(m1.C * x, m3.C);
+            Assert.AreEqual(m1.D * x, m3.D);
+        }
+        
+        [TestMethod]
+        public void Test_Operator_Multiply_Matrix_Right()
+        {
+            var x = 2;
+            var m2 = new Matrix2d(1, 2, 3, 4);
+            var m3 = x * m2;
+            Assert.AreEqual(2, m3.A);
+            Assert.AreEqual(4, m3.B);
+            Assert.AreEqual(6, m3.C);
+            Assert.AreEqual(8, m3.D);
+            Assert.AreEqual(x * m2.A, m3.A);
+            Assert.AreEqual(x * m2.B, m3.B);
+            Assert.AreEqual(x * m2.C, m3.C);
+            Assert.AreEqual(x * m2.D, m3.D);
         }
     }
 }
